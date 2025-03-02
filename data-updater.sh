6b2f2b728732ba0aec934f821bd9f4a1a0625653
Augusto Coronel <aocoronel@proton.me>
2025-03-02 11:44:34 -0300

    scripts: Add a script to update JSON file to v2

---
+++ data-updater.sh
@@ -0,0 +1,13 @@
+#!/bin/env bash
+
+file=$(ls ./data/ | fzf --prompt "Choose a data file to strip: ")
+# Get unique ids from the JSON file
+id=$(jq -r 'map(.id) | unique[]' $file)
+
+# Loop through each unique id
+for i in $id; do
+  echo "Running $i"
+
+  # Filter the $file by id and output to a new file
+  jq --arg id "$i" 'map(select(.id == $id))' $file >"$i".json
+done
