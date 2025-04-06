b048526fa3626acd24e6fa9babaee3565c025e09
Augusto Coronel <aocoronel@proton.me>
2025-04-06 11:26:09 -0300

    feat: Split jsonMappings from index.html

---
+++ mappings.js
@@ -0,0 +1,10 @@
+const jsonMappings = {
+  ai: "data/ai.json",
+  book: "data/book.json",
+  code: "data/code.json",
+  db: "data/db.json",
+  lifestyle: "data/lifestyle.json",
+  news: "data/news.json",
+  software: "data/software.json",
+  yt: "data/yt.json",
+};
